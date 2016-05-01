64a5eb9df4fcc5c885d391a38140ebbf33790981
stephybee <stephybee13@gmail.com>
2016-04-30 23:59:24 -0700

    create constants.h

---
+++ solution--stephybee/constants.h
@@ -0,0 +1,16 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Diem-Huong Bui <diemhuongbui@csu.fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* A header file containing constants.
+*/
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int X = 80;
+const int Y = 25;
+
+#endif
